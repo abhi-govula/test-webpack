1d37a3a173a5c643bbf35d08a402447a0c1e5aa5
abhi.govula <abhi.govula@scriptbees.com>
2017-01-08 19:33:02 +0530

    Initial commit

---
+++ bundle.js
@@ -0,0 +1,69 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+
+
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ function(module, exports) {
+
+	"use strict";
+
+	console.log("hi there from webpack");
+
+	var testVariable = "Test ES6 with template strings";
+	var name = "Johdes";
+	var age = 9000;
+	var newObl = {
+	    name: name,
+	    age: age,
+	    testVariable: testVariable
+	};
+	var secObj = { age: 10000 };
+
+	setTimeout(function () {
+	    return console.log("The message after timeout");
+	}, 2000);
+	var clicked = function clicked() {
+	    console.log(secObj);
+	};
+
+/***/ }
+/******/ ]);
---
+++ js/main.js
@@ -0,0 +1,16 @@
+console.log("hi there from webpack");
+
+const testVariable = `Test ES6 with template strings`;
+const name = "Johdes";
+const age = 9000;
+const newObl = {
+    name,
+    age,
+    testVariable
+}
+const secObj = {age: 10000};
+
+setTimeout(() => console.log("The message after timeout"), 2000);
+const clicked = () => {
+    console.log(secObj);
+}
